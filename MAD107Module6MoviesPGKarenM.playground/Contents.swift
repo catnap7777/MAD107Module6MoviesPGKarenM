0aa31c064385f6cf55728c485094bcc6acf8c2c3
km <35552602+catnap7777@users.noreply.github.com>
2020-02-27 19:12:47 -0600

    code cleanup

--- MAD107Module6MoviesPGKarenM.playground/Contents.swift
+++ MAD107Module6MoviesPGKarenM.playground/Contents.swift
@@ -2,21 +2,9 @@ import Cocoa
 
 var str = "Hello, playground"
 
-//.. Player Dictionary
-//var playersDictionary = ["91": (name:"DRAKE CAGGIULA",age:"25",bday:"Jun 20, 1994",height:"5'10",country:"CAN"),
-//                         "22": (name:"RYAN CARPENTER",age:"29",bday:"Jan 18, 1991",height:"6'0",country:"USA"),
-//                         "77": (name:"KIRBY DACH",age:"19",bday:"Jan 21, 2001",height:"6'4",country:"CAN"),
-//                         "12": (name:"ALEX DEBRINCAT",age:"22",bday:"Dec 18, 1997",height:"5'7",country:"USA"),
-//                         "36": (name:"MATTHEW HIGHMORE",age:"23",bday:"Feb 27, 1996",height:"5'11",country:"CAN"),
-//                         "64": (name:"DAVID KAMPF",age:"25",bday:"Jan 12, 1995",height:"6'2",country:"CZE"),
-//                         "88": (name:"PATRICK KANE",age:"31",bday:"Nov 19, 1988",height:"5'10",country:"USA"),
-//                         "8": (name:"DOMINIK KUBALIK",age:"24",bday:"Aug 21, 1995",height:"6'2",country:"CZE"),
-//                         "30": (name:"MALCOLM SUBBAN",age:"26",bday:"Dec 21, 1993",height:"6'2",country:"CAN")
-//]
-
 //.. Movie Array - array of Tuples
 var movieArray = [(IMDB:"tt0325980", movieName:"Pirates of the Caribbean: The Curse of the Black Pearl",
-                   myRating:"A+", myComments:"Great family movie!"),
+                   myRating:"A", myComments:"Great family movie!"),
                   (IMDB:"tt1201607", movieName:"Harry Potter and the Deathly Hallows: Part 2",
                    myRating:"B", myComments:"Great family movie but the ending didn't totaly follow the book!"),
                   (IMDB:"tt0076759", movieName:"Star Wars: Episode IV - A New Hope",
@@ -24,7 +12,7 @@ var movieArray = [(IMDB:"tt0325980", movieName:"Pirates of the Caribbean: The Cu
                   (IMDB:"tt0120737", movieName:"The Lord of the Rings: The Fellowship of the Ring",
                    myRating:"A+", myComments:"Simply awesome!"),
                   (IMDB:"tt0167261", movieName:"The Lord of the Rings: The Two Towers",
-                   myRating:"B", myComments:"A little slower than the others...!"),
+                   myRating:"A-", myComments:"A little slower than the others...!"),
                   (IMDB:"tt0167260", movieName:"The Lord of the Rings: The Return of the King",
                    myRating:"A+", myComments:"Great finish to the trilogy!"),
                   (IMDB:"tt0120762", movieName:"Mulan",
@@ -35,7 +23,8 @@ var movieArray = [(IMDB:"tt0325980", movieName:"Pirates of the Caribbean: The Cu
                    myRating:"A+", myComments:"One of my favorites! Imo, better than the Colin Firth version"),
                   (IMDB:"tt0320691", movieName:"Underworld",
                    myRating:"B-", myComments:"Great action movie! Good SciFi series. A little bloody though..."),
-                  
+                  (IMDB:"tt0112130", movieName:"Pride and Prejudice",
+                   myRating:"C", myComments:"This is the one with Colin Firth. It's way too long and kind of slow...")
 ]
 
 //.. Used MCH303 Android App to pick off data for this dictionary(key/tuple)  :)
@@ -57,59 +46,47 @@ var movieDictionary = ["tt0325980": (movieName:"Pirates of the Caribbean: The Cu
                                      moviePosterURL:"https://m.media-amazon.com/images/M/MV5BNDc4MThhN2EtZjMzNC00ZDJmLThiZTgtNThlY2UxZWMzNjdkXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg"),
                        "tt0414387": (movieName:"Pride & Prejudice", movieYear:"2005", movieType:"movie",
                                      moviePosterURL:"https://m.media-amazon.com/images/M/MV5BMTA1NDQ3NTcyOTNeQTJeQWpwZ15BbWU3MDA0MzA4MzE@._V1_SX300.jpg"),
+                       "tt0112130": (movieName:"Pride and Prejudice", movieYear:"1995", movieType:"series",
+                                     moviePosterURL:"https://m.media-amazon.com/images/M/MV5BMDM0MjFlOGYtNTg2ZC00MmRkLTg5OTQtM2U5ZjUyYTgxZThiXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"),
                        "tt0320691": (movieName:"Underworld", movieYear:"2003", movieType:"movie",
                                      moviePosterURL:"https://m.media-amazon.com/images/M/MV5BMTk1OTc2ZmUtODU0Yy00NGJiLWJmNmQtODI0MzBjODk3MjI4L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg")
-
 ]
-
-let movieTuple = movieDictionary.values
-let sortedMovieTuple = movieTuple.sorted(by: {$0.movieName < $1.movieName})
-//print(movieTuple)
-print("Here are my movies sorted by name:")
-print("------------------------------------------------------------------------------------------------\n")
-
-
-for item in sortedMovieTuple {
-    print("\t Name: \(item.movieName)")
-    print("\t Year:\(item.movieYear) Type:\(item.movieType)")
-    print("\n................................................................................................\n")
+//....................................................................................................................................................
+func printMyReview() {
+    
+    let movieArrayTuple = movieArray
+    
+    let sortedMovieArrayTuple = movieArrayTuple.sorted(by: {$0.myRating < $1.myRating})
+    //print(movieArrayTuple[0])
+    print("\n\nHere are my movies reviews sort by rating:")
+    print("------------------------------------------------------------------------------------------------")
+    
+    for item in sortedMovieArrayTuple {
+        print("\t Name: \(item.movieName)")
+        print("\t\t My Raing: \(item.myRating)")
+        print("\t\t My Movie Comments: \(item.myComments)")
+        print("................................................................................................")
+    }
 }
-
-
-//let dictValInc = dict.sorted(by: { $0.value < $1.value })
-//let testVar8ValInc = playersDictionary.sorted(by: { $0.value.name < $1.value.name })
-//
-//for (k,v) in testVar8ValInc where v.country == "USA" {
-//    print("\n******* \(v.name) is number \(k) and is \(v.height) tall")
-//}
-//..***************************************************************************************************************************************
-//func sortByAge() {
-//
-//    print("***************************** Sort by Age *****************************************\n")
-//
-//    //let sortedPlayerDictionaryAge = playersDictionary.sorted(by: {$0.value.age < $1.value.age})
-//
-//    //****  this part is the important part!!!!!!
-//    let playersTuple = playersDictionary.values
-//
-//    let sortedTupleByAge = playersTuple.sorted(by: {$0.age < $1.age})
-//    print("\n******* Sorted by Age ********\n")
-//    print("    AGE    PLAYER NAME")
-//    print("    ---    -----------\n")
-//    var counterAge = 0
-//    var counterA = 0
-//    for item in sortedTupleByAge {
-//        //for item in sortedPlayerDictionaryAge {
-//        //print("\t\(item.value.age) | \(item.value.name) ")
-//        print("\t\(item.age) | \(item.name) ")
-//        counterA += 1
-//        counterAge += Int(item.age) ?? 0
-//    }
-//
-//    let averageAge = counterAge/counterA
-//    print("\nThe average age of the players is: \(averageAge)")
-//}
-
+//....................................................................................................................................................
 func printMyMovies() {
     
+    let movieTuple = movieDictionary.values
+    let sortedMovieTuple = movieTuple.sorted(by: {$0.movieName < $1.movieName})
+    //print(movieTuple)
+    print("\n\nHere are my movies sorted by name:")
+    print("------------------------------------------------------------------------------------------------")
+    
+    
+    for item in sortedMovieTuple {
+        print("\t Name: \(item.movieName)")
+        print("\t\t Year: \(item.movieYear) Type: \(item.movieType)")
+        print("................................................................................................")
+    }
+    
 }
+//....................................................................................................................................................
+
+printMyMovies()
+printMyReview()
+
